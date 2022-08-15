aa470100ecf5792a43e72470b54ecbe7ed15abc0
James-favour <108298261+James-favour@users.noreply.github.com>
2022-08-15 13:21:28 +0100

    Add files via upload

---
+++ 15th/me.js
@@ -0,0 +1,56 @@
+// let boy = ['timo', 'john', 'fred'];
+// function func1() {
+//     let output= ''
+//     if (typeof boy != 'undefined') {
+//         output += 'boy:' + boy;
+
+//     }
+// }
+
+// func1();
+// let outfit = 'shirt';
+// function mycloth() {
+//     let outfits = 'skirt';
+//     return outfit;
+
+// }
+// console.log(outfit);
+
+// function minusseven(num) {
+//     return num-6;
+// }
+// console.log(minusseven(15)); 
+// let sum = 0;
+// function addthree() {
+//     sum+= 3;
+//     console.log(sum);
+// }
+// addthree()
+// changed = ''
+// function change(num) {
+//     return (num+5)/2;
+// }
+
+// changed = change(10); 
+// console.log(changed);
+
+// let maths = '';
+// function add(num) {
+//     return (num/5) * 2.5;
+// }
+// maths = add(10);
+// console.log(maths);
+
+// function nextinline(arr,item) {
+//     arr.push(item);
+//     return arr.shift();
+
+//  let test = [0,1,2,3,4,5];
+// console.log('before:' + JSON.stringify(test));
+// console.log((test,6,7,8));
+// console.log('after:' + JSON.stringify(test));
+
+
+
+document.getElementById('going').innerHTML= 'HELLO THERE!';
+ 
